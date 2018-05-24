e1ca0392df988c6c45325e6390dd7fc3d7a0abe5
Peter Staev <peter@tangrasoft.com>
2018-05-24 11:10:58 +0300

    plugin init

---
+++ README.md
@@ -0,0 +1,46 @@
+# NativeScript Photo Editor
+[![Build Status](https://travis-ci.com/PeterStaev/nativescript-photo-editor.svg?branch=master)](https://travis-ci.com/PeterStaev/nativescript-photo-editor)
+[![npm downloads](https://img.shields.io/npm/dm/nativescript-photo-editor.svg)](https://www.npmjs.com/package/nativescript-photo-editor)
+[![npm downloads](https://img.shields.io/npm/dt/nativescript-photo-editor.svg)](https://www.npmjs.com/package/nativescript-photo-editor)
+[![npm](https://img.shields.io/npm/v/nativescript-photo-editor.svg)](https://www.npmjs.com/package/nativescript-photo-editor)
+
+A NativeScript DropDown widget. The DropDown displays items from which the user can select one. For iOS it wraps up a [UILabel](https://developer.apple.com/reference/uikit/uilabel) with an `inputView` set to an [UIPickerView](https://developer.apple.com/library/prerelease/ios/documentation/UIKit/Reference/UIPickerView_Class/index.html) which displays the items. For Android it wraps up the [Spinner](http://developer.android.com/reference/android/widget/Spinner.html) widget.
+
+## Screenshot
+_COMMING SOON!_
+
+## Installation
+Run the following command from the root of your project:
+
+`tns plugin add nativescript-photo-editor`
+
+This command automatically installs the necessary files, as well as stores nativescript-photo-editor as a dependency in your project's `package.json` file.
+
+## Configuration
+There is no additional configuration needed!
+
+## API
+_COMMING SOON!_
+
+## Usage
+_COMMING SOON!_
+
+## Usage in Angular
+There is no difference in usage between Core and Angular. So you can refer to the above usage examples on how to use this plugin with Angular. 
+
+## Demos
+This repository includes a plain NativeScript demo. In order to run it execute the following in your shell:
+```shell
+$ git clone https://github.com/peterstaev/nativescript-photo-editor
+$ cd nativescript-photo-editor
+$ npm install
+$ npm run demo-ios
+```
+This will run the plain NativeScript demo project on iOS. If you want to run it on Android simply use the `-android` instead of the `-ios` sufix. 
+
+## Donate
+[![Donate](https://img.shields.io/badge/paypal-donate-brightgreen.svg)](https://bit.ly/2AS9QKB)
+
+`bitcoin:14fjysmpwLvSsAskvLASw6ek5XfhTzskHC`
+
+![Donate](https://www.tangrainc.com/qr.png)
---
+++ gruntfile.js
@@ -0,0 +1,162 @@
+﻿module.exports = function (grunt) {
+    var localConfig = {
+        typeScriptSrc: [
+            "**/*.ts",
+            "!node_modules/**/*.*",
+            "!demo/**/*.*",
+            "!bin/**/*.*"
+        ],
+        typeScriptDeclarations: [
+            "**/*.d.ts",
+            "!references.d.ts",
+            "!typings/*.*",
+            "!node_modules/**/*.*",
+            "!demo/**/*.*",
+            "!bin/**/*.*"
+        ],
+        outDir: "bin/dist/"
+    }
+
+    grunt.initConfig({
+        clean: {
+            build: {
+                src: [localConfig.outDir]
+            }
+        },
+        ts: {
+            build: {
+                tsconfig: true
+            }
+        },
+        tslint:
+        {
+            build:
+            {
+                src: localConfig.typeScriptSrc.concat(["!typings/*.*"]),
+                options: {
+                    configuration: grunt.file.readJSON("./tslint.json")
+                }
+            }
+        },
+        copy: {
+            declarations: {
+                files: [{ expand: true, src: localConfig.typeScriptDeclarations, dest: localConfig.outDir }]
+            },
+            platforms: {
+                files: [{ expand: true, src: ["platforms/**"], dest: localConfig.outDir }]
+            },
+            packageConfig: {
+                src: "package.json",
+                dest: localConfig.outDir,
+                options: {
+                    process: function (content, srcPath) {
+                        var contentAsObject = JSON.parse(content);
+                        contentAsObject.devDependencies = undefined;
+                        return JSON.stringify(contentAsObject, null, "\t");
+                    }
+                }
+            },
+            readme: {
+                src: "README.md",
+                dest: localConfig.outDir,
+                options: {
+                    process: function (content, srcPath) {
+                        return content.substring(content.indexOf("\n") + 1)
+                    }
+                }
+            },
+            // android_aar: {
+            //     src: "android/inappbilling/inappbillinghelper/build/outputs/aar/inappbillinghelper-release.aar",
+            //     dest: localConfig.outDir + "platforms/android/inappbillinghelper-release.aar"
+            // }
+        },
+        exec: {
+            tsCompile: "npm run tsc -- --outDir " + localConfig.outDir,
+            tslint: "npm run tslint",
+            checkRequiredReadmeSection: {
+                cwd: "bin/dist",
+                cmd: function (section) {
+                    return "cat README.md | grep -q \"# " + section + "\"";
+                }
+            },
+            checkRequiredPackageJsonSection: {
+                cwd: "bin/dist",
+                cmd: function (section) {
+                    return "cat package.json | grep -q \"\\\"" + section + "\\\"\"";
+                }
+            },
+            "ci-build-demo": {
+                cmd: function (platform, demoSuffix) {
+                    return "cd demo" + (demoSuffix != "" ? "-" + demoSuffix : "") + " && npm install && tns build " + platform;
+                }
+            },
+            "ci-webpack-demo": {
+                cmd: function (platform, demoSuffix) {
+                    return "cd demo" + (demoSuffix != "" ? "-" + demoSuffix : "")+ " && npm install && tns build " + platform + " --bundle --env.uglify --env.snapshot";
+                }
+            },
+            // build_android_aar: {
+            //     cmd: "./gradlew build",
+            //     cwd: "android/inappbilling/"
+            // },
+            npm_publish: {
+                cmd: "npm publish",
+                cwd: localConfig.outDir
+            },             
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-exec");
+  
+    grunt.registerTask("compile", function () {
+        var tasks = [
+            "clean:build",
+            "exec:tsCompile",
+            "copy"
+        ];
+        // if (!process.env.iOS) {
+        //     tasks.splice(2, 0, "exec:build_android_aar");
+        // }
+        
+        grunt.task.run(tasks);
+    });
+    
+    grunt.registerTask("build", [
+        "exec:tslint",
+        "compile",
+        "copy"
+    ]);
+
+    grunt.registerTask("ci", "Performs CI builds for the demo projects", function (action, platform) {
+        if (!platform || platform === "") {
+            grunt.warn("You must specify a platform (ios or android)!");
+        }
+        if (!action || action === "") {
+            grunt.warn("You must specify an action (build or webpack)!");
+        }
+
+        var baseTask = "exec:ci-" + action.toLowerCase() + "-demo:" + platform.toLowerCase();
+        grunt.task.run(
+            baseTask + ":"
+            // baseTask + ":ng"
+        );
+    });
+
+    grunt.registerTask("lint", [
+        "exec:checkRequiredReadmeSection:Installation",
+        "exec:checkRequiredReadmeSection:Configuration",
+        "exec:checkRequiredReadmeSection:API",
+        "exec:checkRequiredReadmeSection:Usage",
+        "exec:checkRequiredPackageJsonSection:license",
+        "exec:checkRequiredPackageJsonSection:nativescript",
+        "exec:tslint",
+    ]);
+
+    grunt.registerTask("publish", [
+        "build",
+        "lint",
+        "exec:npm_publish"
+    ]);
+};
